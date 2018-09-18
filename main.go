470d7777c1b4710d6ac3196f4826e3288a4fcc65
soudegesu <soudegesu@hotmail.co.jp>
2018-09-18 09:32:04 +0900

    define some routing path

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Welcome")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Todo show: %s", ps.ByName("todoId"))
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		print(w)
-	})
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/todos", TodoIndex)
+	router.GET("/todos/:todoId", TodoShow)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
