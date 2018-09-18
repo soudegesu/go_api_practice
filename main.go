28fdb7b03cd5c1df1a297a563eaffc139dd2ef0f
soudegesu <soudegesu@hotmail.co.jp>
2018-09-18 10:06:35 +0900

    separate modules

--- main.go
+++ main.go
@@ -3,35 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
-	"fmt"
-	"time"
-	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 )
-
-type Todo struct {
-	Name string `json:"name"`
-	Completed bool `json:"completed"`
-	Due time.Time `json:"due"`
-}
-
-type Todos []Todo
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Welcome")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	todos := Todos {
-		Todo {Name: "Write presentation"},
-		Todo {Name: "Host meetup"},
-	}
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Todo show: %s", ps.ByName("todoId"))
-}
 
 func main() {
 	router := httprouter.New()
